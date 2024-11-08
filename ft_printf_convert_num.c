89e417165f67010ac625d43bcc5ca8f469df86fa
Ganesha O'Donnell <go-donne@student.42heilbronn.de>
2024-11-08 11:08:27 +0100

    Completion of implementation of integer conversion handler

--- ft_printf_convert_num.c
+++ ft_printf_convert_num.c
@@ -6,14 +6,14 @@
 /*   By: go-donne <go-donne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 09:30:45 by go-donne          #+#    #+#             */
-/*   Updated: 2024/11/08 11:01:01 by go-donne         ###   ########.fr       */
+/*   Updated: 2024/11/08 11:07:55 by go-donne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 static int	write_char_safely(char c)
 {
 	if (write(1, &c, 1) == -1)
-		return(-1);
+		return (-1);
 	return (1);
 }
 
@@ -56,6 +56,29 @@ static int	write_number_recursive(int n)
 	}
 	digit = (n % 10) + '0';
 	write_result = write_char_safely(digit);
+	if (write_result == -1)
+		return (-1);
+	return (chars_written + write_result);
+}
+
+int	handle_integer(va_list args)
+{
+	int	number;
+	int	chars_written;
+	int	write_result;
+
+	number = va_arg(args, int);
+	chars_written = 0;
+	if (number == -2147483648)
+		return (write_string_safely("-2147483648"));
+	if (number < 0)
+	{
+		write_result = handle_negative(&number);
+		if (write_result == -1)
+			return (-1);
+		chars_written += write_result;
+	}
+	write_result = write_number_recursive(number);
 	if (write_result == -1)
 		return (-1);
 	return (chars_written + write_result);
